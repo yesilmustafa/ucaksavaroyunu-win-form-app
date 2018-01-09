02ba9d75709059b96fdf6f3e0969955e68a051f5
Mustafa <mustafayesil.ybs@gmail.com>
2018-01-09 21:39:28 +0300

    Initial commit

---
+++ ucak_savar/ucak_savar/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace ucak_savar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.ucaksavar = new System.Windows.Forms.PictureBox();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ucaksavar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 50);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(765, 97);
+            this.panel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.BlanchedAlmond;
+            this.label5.Location = new System.Drawing.Point(686, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.BlanchedAlmond;
+            this.label4.Location = new System.Drawing.Point(629, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Puan:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(5, 60);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(271, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ateş etmek için BOŞLUK tuşuna basın.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(4, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(456, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Uçak Savarı Hareket Ettirmek için Sağ Sol Yön Tuşlarını Kullanın.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(5, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Oyunu Başlatmak için Enter Tuşuna Basın.";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 50;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1750;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // ucaksavar
+            // 
+            this.ucaksavar.Image = ((System.Drawing.Image)(resources.GetObject("ucaksavar.Image")));
+            this.ucaksavar.Location = new System.Drawing.Point(340, 739);
+            this.ucaksavar.Name = "ucaksavar";
+            this.ucaksavar.Size = new System.Drawing.Size(70, 70);
+            this.ucaksavar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ucaksavar.TabIndex = 1;
+            this.ucaksavar.TabStop = false;
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(734, 811);
+            this.Controls.Add(this.ucaksavar);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Uçak Savar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ucaksavar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox ucaksavar;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ ucak_savar/ucak_savar/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ucak_savar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int yer;
+        bool a = false;
+        Random rnd = new Random();
+        List<PictureBox> ucaklar = new List<PictureBox>();
+        List<PictureBox> mermiler = new List<PictureBox>();
+        int puan =0;
+        private void mermileriolustur()
+        {
+            PictureBox mermi = new PictureBox();
+            mermi.Size = new Size(30, 30);
+            mermi.SizeMode = PictureBoxSizeMode.StretchImage;
+            mermi.ImageLocation = "png/mermi.png";
+            int mermilocx = ucaksavar.Location.X + 18;
+            int mermilocy = ucaksavar.Location.Y - 20;
+            mermi.Location = new Point(mermilocx, mermilocy);
+            this.Controls.Add(mermi);
+            mermiler.Add(mermi);
+        }
+        private void mermilerifirlat()
+        {
+           
+                for (int i = 0; i < mermiler.Count; i++)
+                {
+                    mermiler[i].Top = mermiler[i].Top - 4;
+           
+                }
+          
+        }
+        private void oyunu_bitir()
+        {
+            PictureBox yanma = new PictureBox();
+            yanma.Size = new Size(50, 50);
+            yanma.SizeMode = PictureBoxSizeMode.StretchImage;
+            yanma.ImageLocation = "png/yanma.png";
+
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            for (int t = 0; t < mermiler.Count; t++)
+            {
+                mermiler[t].Top = 0;
+                this.Controls.Remove(mermiler[t]);
+
+            }
+            for (int j = 0; j < ucaklar.Count; j++)
+            {
+                ucaklar[j].Top = 0;
+                this.Controls.Remove(ucaklar[j]);
+
+
+            }
+            ucaklar.Clear();
+            mermiler.Clear();
+            //Ram temizleme
+            GC.Collect(); //Çöp Toplayıcısı
+            GC.WaitForPendingFinalizers();  //Çöp Yakıcısı
+            MessageBox.Show("Oyun Bitti. Puanınız..:" + puan.ToString());
+            puan = 0;
+            label5.Text = puan.ToString();
+
+        }
+
+        private void ucaklariolustur()
+        {
+            yer = rnd.Next(0, 680);
+            PictureBox ucak = new PictureBox();  //Düşecek nesne picturebox olacak
+            ucak.Size = new Size(50, 50);    // Düşecek nesnenin boyutu
+            ucak.SizeMode = PictureBoxSizeMode.StretchImage;  // Resmin genişliği
+            ucak.ImageLocation = "png/ucak.png"; // Picturebox'ın içine gilecek resmin adresi
+            ucak.Location = new Point(yer, 50);  // Picturebox'ın forumdaki koordinatları
+            ucak.Visible = true;
+            this.Controls.Add(ucak);  //
+            ucaklar.Add(ucak);
+        }
+        private void ucaklaridusur()
+        {
+            for (int i = 0; i < ucaklar.Count; i++)
+            {
+                    ucaklar[i].Top = ucaklar[i].Top + 5;
+               
+                    if (ucaklar[i].Location.Y >= 760 ||  (ucaklar[i].Bounds.IntersectsWith(ucaksavar.Bounds)))//ucakların ucaksavara çarpma durumu
+                    {
+                        oyunu_bitir();
+                    }
+                
+            }
+        }
+     
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            mermilerifirlat();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            ucaklaridusur();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            ucaklariolustur();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int xkoordinati = ucaksavar.Location.X;
+            int ykoordinati = ucaksavar.Location.Y;
+            if (e.KeyCode == Keys.Right) //  Klavyeden Sol Yön Tuşuna Basıldığında Uçaksavarın Sola Gitmesi İçin
+            {
+                xkoordinati = xkoordinati + 9;
+            }
+            if (e.KeyCode == Keys.Left)   // Klavyeden Sağ Yön Tuşuna Basıldığında Uçaksavarın Sağa Gitmesi İçin
+            {
+                xkoordinati = xkoordinati - 9;
+            }
+            if (xkoordinati >= 660)   // Uçaksavarın Form Ekranından Çıkmaması İçin
+            {
+                xkoordinati = 660;
+            }
+            else if (xkoordinati <= 0)
+            {
+                xkoordinati = 0;
+            }
+            ucaksavar.Location = new Point(xkoordinati, ykoordinati);
+
+            if (e.KeyCode == Keys.Space)
+            {
+                timer1.Start();
+                mermileriolustur();
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                timer3.Start();
+                timer2.Start();
+                timer4.Start();
+             }
+        }
+
+
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+             for (int i = 0; i < mermiler.Count; i++)
+             {
+                for (int j = 0; j < ucaklar.Count; j++)
+                {
+                    try
+                    {
+                       
+                        if (mermiler[i].Bounds.IntersectsWith(ucaklar[j].Bounds))//mermiler ile uçaklar çarpıştımı
+                        {
+                            puan += 5;
+                            if (puan >= 50)
+                            {
+                                timer3.Interval = 1500;
+                            }
+                            else if (puan >= 100)
+                            {
+                                timer3.Interval = 1000;
+                            }
+                            else if (puan >= 200)
+                            {
+                                timer3.Interval = 750;
+                            }
+                            else if (puan >= 350)
+                            {
+                                timer3.Interval = 500;
+                            }
+                            label5.Text = puan.ToString();
+
+                            this.Controls.Remove(mermiler[i]);
+                            mermiler.Remove(mermiler[i]);
+
+                            this.Controls.Remove(ucaklar[j]);
+                            ucaklar.Remove(ucaklar[j]);
+                            //Ram temizleme
+                            GC.Collect(); //Çöp Toplayıcısı
+                            GC.WaitForPendingFinalizers();  //Çöp Yakıcısı
+                        }
+                    }
+                    catch (ArgumentOutOfRangeException)//picturebox listesinde eleman silindiği için aynı indis tekrar kontrol edildiğinde ilk aşamada null değer olduğu için return yaparak görmezden geliyoruz
+                    {
+
+                        return;
+                    }
+                }
+
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
